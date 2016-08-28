880855551715eec31741c01f38cec89018c40bb4
Jan Greger Hemb <jan.greger@gmail.com>
2016-08-28 20:22:55 +0200

    Minor extra code on angularJS

--- take3 - MEAN/server1 Angular/public/js/app.js
+++ take3 - MEAN/server1 Angular/public/js/app.js
@@ -4,4 +4,17 @@ angular.module("TestApp").controller("MainController", mainCtrlFunc);
 
 function mainCtrlFunc(){
 	this.message = "Hello";
+
+	this.people = [
+		{
+			name: "Greger"
+		},
+		{
+			name: "Hanne"
+		},
+		{
+			name: "Markus"
+		}
+	];
+
 }
